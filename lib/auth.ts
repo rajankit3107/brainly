0a4e98e9cbb8eb6e1b73005716ea66fd528881f2
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-10-22 01:40:18 +0530

    feat : added better-auth with emailAndPassword

---
+++ lib/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "./generated/prisma";
+import { nextCookies } from "better-auth/next-js";
+
+const prisma = new PrismaClient();
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, { provider: "postgresql" }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: "",
+      clientSecret: "",
+    },
+    google: {
+      clientId: "",
+      clientSecret: "",
+    },
+  },
+  plugins: [nextCookies()],
+});
